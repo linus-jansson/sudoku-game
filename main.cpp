a3ed22d4c5e2ded25f8f29595b58668d872936f9
Linus <linus.jansson2000@gmail.com>
2020-05-05 14:05:22 +0200

    "Beautify"

--- main.cpp
+++ main.cpp
@@ -4,21 +4,19 @@
 #include <random>
 #include <iterator>
 
-
 #include "Sudoku.cpp"
 
 // typedef std::vector<int> rad;
 // typedef std::vector<rad> matrix;
 
-void clear() {
+void clear()
+{
     // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
     std::cout << "\x1B[2J\x1B[H";
 }
 
-
 int main()
 {
-
 
     int menuOption;
     char option;
@@ -26,11 +24,11 @@ int main()
     bool editing;
 
     do
-    {   
+    {
         // clear();
         std::cout << "What do you want to do?"
                   << "\n1. Generate a Sudoku board"
-                //   << "\n2. Fill in empty board"
+                  //   << "\n2. Fill in empty board"
                   << "\n0. Exit\n";
         std::cin >> menuOption;
 
@@ -41,86 +39,81 @@ int main()
         switch (menuOption)
         {
         case 1:
-                clear();
-                std::cout << "Before solved: \n";
-                s.display();
+            clear();
+            std::cout << "Before solved: \n";
+            s.display();
 
-                std::cout << "Do you want to solve the board? Y or N\n";
-                std::cin >> option;
-                
-                switch (option)
+            std::cout << "Do you want to solve the board? Y or N\n";
+            std::cin >> option;
+
+            switch (option)
+            {
+            case 'Y':
+            case 'y':
+                if (s.solve())
                 {
-                case 'Y':
-                case 'y':
-                    if (s.solve())
-                    {
-                        clear();
-                        std::cout << "Before solved: \n";
-                        copyBoard.display();
-                        std::cout << "Solved: \n";
-                        s.display();
-                    }
-                    else
-                    {
-
-                        std::cout << "No solution.\n";
-                        
-                    }
-                    break;
-                
-                default:
-                    break;
+                    clear();
+                    std::cout << "Before solved: \n";
+                    copyBoard.display();
+                    std::cout << "Solved: \n";
+                    s.display();
                 }
+                else
+                {
 
+                    std::cout << "No solution.\n";
+                }
+                break;
+
+            default:
+                break;
+            }
 
             break;
-        // case 2:
-        //     editing = true;
-        //     s.emptyGrid();
-            
-        //     char editInput;
-        //     while(editing)
-        //     {
-        //         std::cout << "Editing \n";
-        //         std::cin >> editInput;
-        //         s.display();
-        //         switch (editInput)
-        //         {
-        //         case 'q':
-        //         case 'Q':
-        //         case 's':
-        //         case 'S':
-        //             // Saves the current grid as it is
-        //             editing = false;
-        //             break;
-        //         case 'A':
-        //         case 'a':
-        //             // Moves selection to the left
-        //             break;
-                
-        //         case 'D':
-        //         case 'd':
-        //             // Moves selection to the right
-        //             break;
-                    
-        //         default:
-                    
-        //             break;
-        //         }
-        //     }
-        //     continue;
-            
-            
+            // case 2:
+            //     editing = true;
+            //     s.emptyGrid();
+
+            //     char editInput;
+            //     while(editing)
+            //     {
+            //         std::cout << "Editing \n";
+            //         std::cin >> editInput;
+            //         s.display();
+            //         switch (editInput)
+            //         {
+            //         case 'q':
+            //         case 'Q':
+            //         case 's':
+            //         case 'S':
+            //             // Saves the current grid as it is
+            //             editing = false;
+            //             break;
+            //         case 'A':
+            //         case 'a':
+            //             // Moves selection to the left
+            //             break;
+
+            //         case 'D':
+            //         case 'd':
+            //             // Moves selection to the right
+            //             break;
+
+            //         default:
+
+            //             break;
+            //         }
+            //     }
+            //     continue;
+
         case 0:
             running = false;
 
         default:
             break;
         }
-        
-    } while(running == true);
+
+    } while (running == true);
 
     return 0;
-
-
 }
